307332605f98871543453299c286b08b4f078db5
MustafaTopal <mumusti28@gmail.com>
2024-03-10 05:31:02 +0100

    Firstly, we retrieve the current time dynamically

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Hello World!
+
+function displayCurrentHour () {
+    let date = new Date();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let currentHour = hour + ":" + minutes + ":" + seconds;
+
+    console.log("Current time : " + currentHour)
+}
+
+displayCurrentHour()
